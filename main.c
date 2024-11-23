a2ff304a7077adbf8ce5813ccb75ef7ca5b0bafa
Tharunxm <tharunnacc@gmail.com>
2024-11-23 18:37:29 +0530

    FIXED ERROR WHILE PRINTING THE PAIR

--- main.c
+++ main.c
@@ -1,24 +1,34 @@
 #include <stdio.h>
 
-int main(void){
-    int equivalent_sum,size;
-    printf("Enter the size of array:");
-    scanf("%d",&size);
-    printf("Enter the num:");
-    scanf("%d",&equivalent_sum);
+int main(void) {
+    int equivalent_sum, size;
+    printf("Enter the size of array: ");
+    scanf("%d", &size);
+    printf("Enter the num: ");
+    scanf("%d", &equivalent_sum);
     int nums[size];
     int array_indices[size][2];
-    for(int x=0;x<size;x++){
-        for(int y=x+1;y<size;y++){
-           if(nums[x]+nums[y]==equivalent_sum){
-             array_indices[x][0]=x,array_indices[x][1];
-           }
+    int pair_count = 0;
+    for (int a = 0; a < size; a++) {
+        printf("Enter a Numerical Value: ");
+        scanf("%d", &nums[a]);
+    }
+    for (int x = 0; x < size; x++) {
+        for (int y = x + 1; y < size; y++) {
+            if (nums[x] + nums[y] == equivalent_sum) {
+                array_indices[pair_count][0] = x;
+                array_indices[pair_count][1] = y;
+                pair_count++; 
+            }
         }
     }
-    for(int m=0;m<size;m++){
-        for(int n=0;n<2;n++){
-            print("%d",array_indices[m][n]);
+    if (pair_count > 0) {
+        printf("Pairs of indices:\n");
+        for (int m = 0; m < pair_count; m++) {
+            printf("[%d,%d]\n", array_indices[m][0], array_indices[m][1]);
         }
-        printf("\n");
+    } else {
+        printf("No pairs found.\n");
     }
+    return 0;
 }
